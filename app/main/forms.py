2db8fda7f86fd73709bb9e580c3b2e044b5e9360
utkuumur <uacikalin@etu.edu.tr>
2020-03-20 21:18:22 +0300

    Adding password field

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,5 +6,6 @@ from wtforms.validators import Required
 class LoginForm(Form):
     """Accepts a nickname and a room."""
     name = StringField('Name', validators=[Required()])
+    passwd = StringField('Password', validators=[Required()])
     room = StringField('Room', validators=[Required()])
     submit = SubmitField('Enter Chatroom')
