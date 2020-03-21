cc5f1bbe09898694a79f57efc14ba6cd0e6f022c
Utku <uuacikalin@gmail.com>
2020-03-21 16:14:53 +0300

    encryption start

--- app/main/events.py
+++ app/main/events.py
@@ -1,5 +1,12 @@
 from flask import session
 from flask_socketio import emit, join_room, leave_room
+from flask_common.crypto import (
+    AuthenticationError,
+    EncryptionError,
+    aes_decrypt,
+    aes_encrypt,
+    aes_generate_key,
+)
 from .. import socketio
 
 
@@ -17,8 +24,26 @@ def text(message):
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
     room = session.get('room')
+    #key = aes_generate_key()
+    key = b'utkuumurutkuumurutkuumurutkuumurutkuumurutkuumurutkuumurutkuumur'
+    print('len:',len(key))
+    print('key:',key)
+    enc_msg = aes_encrypt(key, b'test')
+    emit('enc_msg', {'msg': enc_msg}, room=room)
     emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
 
+
+
+# Encrypt + sign using provided IV.
+# Note: You should normally use aes_encrypt().
+def aes_encrypt_iv(key, data, iv):
+    aes_key = b'utkuumurutkuumurutkuumurutkuumur'
+    iv = b'0000'
+    encryptor = Cipher(
+        algorithms.AES(aes_key), modes.CTR(iv), backend=backend
+    ).encryptor()
+    cipher = encryptor.update(data) + encryptor.finalize()
+    return cipher
 
 @socketio.on('left', namespace='/chat')
 def left(message):
