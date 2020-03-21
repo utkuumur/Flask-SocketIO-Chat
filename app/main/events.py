e1c2a4344a67d8affbf3659226e9d997f4c2987d
Utku <uuacikalin@gmail.com>
2020-03-21 23:27:46 +0300

    encryption completed

--- app/main/events.py
+++ app/main/events.py
@@ -10,6 +10,9 @@ from .. import socketio
 
 BLOCK_SIZE = 128 // 8
 
+key = b"1234567890123456"
+hmac_key = b"1234567890123456"
+
 
 @socketio.on('joined', namespace='/chat')
 def joined(message):
@@ -30,7 +33,7 @@ def unpad(data):
 def verify_hmac(cipher,mac, hmac_key):
     local_hash = HMAC.new(hmac_key, digestmod=SHA256)
     local_hash.update(cipher)
-    local_digest = _hash.digest()
+    local_digest = local_hash.digest()
 
     return SHA256.new(mac).digest() == SHA256.new(local_digest).digest()
 
@@ -64,10 +67,18 @@ def text(message):
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
     room = session.get('room')
-    emit('message', {
-                     'cipher': message['msg'],
-                     'msg': session.get('name')
-                    }, room=room)
+    enc = base64.b64decode(message['msg'])
+    IV = enc[:BLOCK_SIZE]
+    hmac = enc[-32:]
+    cipher = enc[16:-32]
+
+
+    if verify_hmac((IV+cipher), hmac, hmac_key):
+        emit('message', {
+                         'cipher':  base64.b64encode((IV+cipher)).decode(),
+                         'mac' : base64.b64encode(hmac).decode(),
+                         'msg': session.get('name')
+                        }, room=room)
 
     '''
     key = os.urandom(32)
@@ -89,19 +100,6 @@ def text(message):
                     }, room=room)
     #emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
     '''
-
-    
-
-# Encrypt + sign using provided IV.
-# Note: You should normally use aes_encrypt().
-def aes_encrypt_iv(key, data, iv):
-    aes_key = b'utkuumurutkuumurutkuumurutkuumur'
-    iv = b'0000'
-    encryptor = Cipher(
-        algorithms.AES(aes_key), modes.CTR(iv), backend=backend
-    ).encryptor()
-    cipher = encryptor.update(data) + encryptor.finalize()
-    return cipher
 
 @socketio.on('left', namespace='/chat')
 def left(message):
